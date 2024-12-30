24a11a933fe92fce0748e424a1fd9204c11e8a17
Eriwe <11926638+zakingslayerv22@users.noreply.github.com>
2024-12-30 09:44:01 -0800

    Start preparing for search results
    Add a main-container element
    in the index.html. This container
    would contain all elements for
    rendering the search results.
    
    Select the mainContainer element
    in the constructor in the
    weather-module GetWeather class
    so that other methods can access
    it.
    
    Initialize a
    buildLeftSectionForSearchResults()
    method that creates all the
    elements for the left
    section that will render
    the search results, gives them
    appropriate classes,
    attributes and append them
    to their respective parents.
    
    Even though
    buildLeftSectionForSearchResults()
    is curently called in initialize(),
    as development of the app
    progress it won't be called
    here. It will be called
    by a method that will
    populate these elements
    with the respective weather
    data.

--- src/weather.module.js
+++ src/weather.module.js
@@ -1,12 +1,14 @@
 export class GetWeather {
   constructor(location) {
     this.location = location;
+    this.mainContainer = document.querySelector(".main-container");
     this.initialize();
   }
 
   initialize() {
     this.getWeatherByLocation();
     this.handleWeatherData();
+    this.buildLeftSectionForSearchResults();
   }
 
   prepareUrl() {
@@ -67,5 +69,118 @@ export class GetWeather {
     } catch (error) {
       console.log(error);
     }
+  }
+
+  buildLeftSectionForSearchResults() {
+    this.mainContainer.textContent = "";
+
+    const searchResultsContainer = document.createElement("div");
+    searchResultsContainer.classList.add("search-results-container");
+
+    const leftContainer = document.createElement("div");
+    leftContainer.classList.add("left-container");
+
+    const homeAndSearchContainer = document.createElement("div");
+    homeAndSearchContainer.classList.add("home-and-search-container");
+
+    const homeButton = document.createElement("div");
+    homeButton.classList.add("home-button");
+
+    const searchContainer = document.createElement("div");
+    searchContainer.classList.add("search-container");
+
+    const searchField = document.createElement("input");
+    searchField.setAttribute("type", "search");
+    searchField.setAttribute("id", "search");
+    searchField.setAttribute("placeholder", "Search by location");
+
+    const searchButton = document.createElement("button");
+    searchButton.textContent = "Search";
+    searchButton.classList.add("search-button");
+
+    const infoContainerLeft = document.createElement("div");
+    infoContainerLeft.classList.add("info-container-left");
+
+    const topContainer = document.createElement("div");
+    topContainer.classList.add("top-container");
+
+    const bottomContainer = document.createElement("div");
+    bottomContainer.classList.add("bottom-container");
+
+    const addressAndTimeContainer = document.createElement("div");
+    addressAndTimeContainer.classList.add("address-and-time-container");
+
+    const addressDiv = document.createElement("div");
+    addressDiv.classList.add("address-div");
+
+    const timeDiv = document.createElement("div");
+    timeDiv.classList.add("time-div");
+
+    const dateDiv = document.createElement("div");
+    dateDiv.classList.add("date-div");
+
+    const weatherDescriptionDiv = document.createElement("div");
+    weatherDescriptionDiv.classList.add("weather-description-div");
+
+    const temperatureAndIconContainer = document.createElement("div");
+    temperatureAndIconContainer.classList.add("temperature-and-icon-container");
+
+    const temperatureDiv = document.createElement("div");
+    temperatureDiv.classList.add("temperature");
+
+    const iconContainer = document.createElement("div");
+    iconContainer.classList.add("icon-container");
+
+    const minAndMaxAndConditionContainer = document.createElement("div");
+    minAndMaxAndConditionContainer.classList.add(
+      "min-max-and-condition-container",
+    );
+
+    const minAndMaxTemperatureContainer = document.createElement("div");
+    minAndMaxTemperatureContainer.classList.add(
+      "min-max-temperature-container",
+    );
+
+    const minTemperatureDiv = document.createElement("div");
+    minTemperatureDiv.classList.add("min-temp-div");
+
+    const maxTemperatureDiv = document.createElement("div");
+    maxTemperatureDiv.classList.add("max-temp-div");
+
+    const conditionsDiv = document.createElement("div");
+    conditionsDiv.classList.add("conditions-div");
+
+    //append
+
+    this.mainContainer.append(searchResultsContainer);
+    searchResultsContainer.append(leftContainer);
+    leftContainer.append(homeAndSearchContainer, infoContainerLeft);
+
+    homeAndSearchContainer.append(homeButton, searchContainer);
+    searchContainer.append(searchField, searchButton);
+
+    infoContainerLeft.append(topContainer, bottomContainer);
+
+    topContainer.append(
+      addressAndTimeContainer,
+      dateDiv,
+      weatherDescriptionDiv,
+    );
+
+    bottomContainer.append(
+      temperatureAndIconContainer,
+      minAndMaxAndConditionContainer,
+    );
+
+    temperatureAndIconContainer.append(temperatureDiv, iconContainer);
+
+    addressAndTimeContainer.append(addressDiv, timeDiv);
+
+    minAndMaxAndConditionContainer.append(
+      minAndMaxTemperatureContainer,
+      conditionsDiv,
+    );
+
+    minAndMaxTemperatureContainer.append(minTemperatureDiv, maxTemperatureDiv);
   }
 }
